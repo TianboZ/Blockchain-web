5e6dc48dd65b726acb5ee1f4eed6974e19ffed85
zongtb <zongtb@126.com>
2018-09-02 11:17:44 -0700

    IPFS initial

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import logo from '../assets/images/logo.svg'
+import DApp from "./DApp";
 
 class Header extends React.Component {
     render() {
         return (
-            <header
-                className="App-header"
-            >
-                <img
-                    src={logo}
-                    className="App-logo" alt="logo"
-                />
-                <h1
-                    className="App-title"
+            <div>
+                <header
+                    className="App-header"
                 >
-                    Project
-                </h1>
-            </header>
+                    <img
+                        src={logo}
+                        className="App-logo" alt="logo"
+                    />
+                    <h1
+                        className="App-title"
+                    >
+                        Project
+                    </h1>
+                </header>
+                <DApp />
+            </div>
+
         );
     }
 }
